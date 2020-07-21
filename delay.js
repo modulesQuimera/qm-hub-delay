465f048efbd7b8a37529aba9b617dde63f14fe82
STRNEOH <strneoh@localhost.localdomain>
2020-07-21 16:06:17 -0300

    adding description

--- delay.js
+++ delay.js
@@ -9,19 +9,32 @@ module.exports = function(RED) {
         // var exportMode = globalContext.get("exportMode");
         var currentMode = globalContext.get("currentMode");
         var command =  {
-          type: "processing_modular_V1.0",
+          type: "processing_modular_V1_0",
           slot: 1,
           compare: {},
           method: "delay",
-          seconds: parseFloat(node.seconds)
+          seconds: parseFloat(node.seconds),
+          get_output: {},
         }
         var file = globalContext.get("exportFile")
         var slot = globalContext.get("slot");
-        if (currentMode == "test") {
-          file.slots[slot].jig_test.push(command)
+        if(!(slot === "begin" || slot === "end")){
+          if(currentMode == "test"){
+              file.slots[slot].jig_test.push(command);
+          }
+          else{
+              file.slots[slot].jig_error.push(command);
+          }
         }
-        else { 
-          file.slots[slot].jig_error.push(command)
+        else{
+          if(slot === "begin"){
+            file.slots[0].jig_test.push(command);
+              // file.begin.push(command);
+          }
+          else{
+            file.slots[3].jig_test.push(command);
+              // file.end.push(command);
+          }
         }
         globalContext.set("exportFile", file);
         node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
